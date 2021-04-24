a8890254434827b9c668d35e4b9dff5f189f05b6
JDFIELD <juanzm27@gmail.com>
2021-04-24 10:35:16 -0500

    funciones

--- lib/page/home-page.dart
+++ lib/page/home-page.dart
@@ -7,7 +7,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<HomePage> {
-  
+  String operaciones = "";
+  bool estado=false;
+
   @override
   Widget build(BuildContext context) {
    
@@ -30,6 +32,11 @@ class _MyHomePageState extends State<HomePage> {
         Container(
           color: Colors.blue,
           height: 100,
+          child: Row(
+            children: [
+              Text(operaciones),
+            ],
+          ),
         ),
         Container(
           child: Column(
@@ -37,36 +44,172 @@ class _MyHomePageState extends State<HomePage> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  ElevatedButton(onPressed: () {}, child: Text("7") ),
-                  ElevatedButton(onPressed: () {}, child: Text("8")),
-                  ElevatedButton(onPressed: () {}, child: Text("9")),
-                  ElevatedButton(onPressed: () {}, child: Text("/")),
+                  ElevatedButton(onPressed: () {
+                    setState(() {
+                      if(estado==false){
+                        operaciones="";
+                        estado=true;
+                      }
+                      operaciones += "7";
+                      
+                    });
+                  }, child: Text("7") ),
+                  ElevatedButton(onPressed: () {
+                    setState(() {
+                      if(estado==false){
+                        operaciones="";
+                        estado=true;
+                      }
+                      operaciones += "8";
+                    });
+                  }, child: Text("8")),
+                  ElevatedButton(onPressed: () {
+                    setState(() {
+                      if(estado==false){
+                        operaciones="";
+                        estado=true;
+                      }
+                      operaciones += "9";
+                    });
+                  }, child: Text("9")),
+                  ElevatedButton(onPressed: () {
+                    setState(() {
+                      operaciones += " / ";
+                      estado=true;
+                    });
+                  }, child: Text("/")),
                 ]
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  ElevatedButton(onPressed: () {}, child: Text("4") ),
-                  ElevatedButton(onPressed: () {}, child: Text("5")),
-                  ElevatedButton(onPressed: () {}, child: Text("6")),
-                  ElevatedButton(onPressed: () {}, child: Text("*")),
+                  ElevatedButton(onPressed: () {
+                    setState(() {
+                      if(estado==false){
+                        operaciones="";
+                        estado=true;
+                      }
+                      operaciones += "4";
+                    });
+                  }, child: Text("4") ),
+                  ElevatedButton(onPressed: () {
+                    setState(() {
+                      if(estado==false){
+                        operaciones="";
+                        estado=true;
+                      }
+                      operaciones += "5";
+                    });
+                  }, child: Text("5")),
+                  ElevatedButton(onPressed: () {
+                    setState(() {
+                      if(estado==false){
+                        operaciones="";
+                        estado=true;
+                      }
+                      operaciones += "6";
+                    });
+                  }, child: Text("6")),
+                  ElevatedButton(onPressed: () {
+                    setState(() {
+                      operaciones += " * ";
+                      estado=true;
+                    });
+                  }, child: Text("*")),
                 ]
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  ElevatedButton(onPressed: () {}, child: Text("1") ),
-                  ElevatedButton(onPressed: () {}, child: Text("2")),
-                  ElevatedButton(onPressed: () {}, child: Text("3")),
-                  ElevatedButton(onPressed: () {}, child: Text("-")),
+                  ElevatedButton(onPressed: () {
+                    setState(() {
+                      if(estado==false){
+                        operaciones="";
+                        estado=true;
+                      }
+                      operaciones += "1";
+                    });
+                  }, child: Text("1") ),
+                  ElevatedButton(onPressed: () {
+                    setState(() {
+                      if(estado==false){
+                        operaciones="";
+                        estado=true;
+                      }
+                      operaciones += "2";
+                    });
+                  }, child: Text("2")),
+                  ElevatedButton(onPressed: () {
+                  setState(() {
+                      if(estado==false){
+                        operaciones="";
+                        estado=true;
+                      }
+                      operaciones += "3";
+                    });
+                  }, child: Text("3")),
+                  ElevatedButton(onPressed: () {
+                  setState(() {
+                      operaciones += " - ";
+                      estado=true;
+                    });
+                  }, child: Text("-")),
                 ]
               ),Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  ElevatedButton(onPressed: () {}, child: Text("0") ),
-                  ElevatedButton(onPressed: () {}, child: Text("C")),
-                  ElevatedButton(onPressed: () {}, child: Text("=")),
-                  ElevatedButton(onPressed: () {}, child: Text("+")),
+                  ElevatedButton(onPressed: () {
+                    setState(() {
+                      if(estado==false){
+                        operaciones="";
+                        estado=true;
+                      }
+                      operaciones += "0";
+                    });
+                  }, child: Text("0") ),
+                  ElevatedButton(onPressed: () {
+                    setState(() {
+                      operaciones= "";
+                      estado=true;
+                  });}, child: Text("C")),
+                  ElevatedButton(onPressed: () {
+                    setState(() {
+                      
+                      var operaciones2= operaciones.replaceAll("-", ",").replaceAll("+", ",").replaceAll("*", ",").replaceAll("/", ",").replaceAll(" ", "");
+                      var array=operaciones2.split(",");
+                      var valores1 = operaciones.replaceAll(" ", ",");
+                      var valores = valores1.split(',') ;
+                      print(valores);
+                      var total;
+                      print(array);
+    print("tamaño");
+    print(valores.length);
+                      for (var i = 2; i <valores.length;i++) {
+                        
+                      if (i==2) {
+                          total= casos(int.parse(valores[0]),valores[1],int.parse(valores[2]));
+                        i=i+1;
+                        }
+                      if(i>2 && i<valores.length){
+                        
+                        total=casos(total,valores[i],int.parse(valores[i+1]));
+                     i=i+1;
+                        
+                      }
+
+
+                      }
+                      operaciones=total.toString();
+                      estado=false;
+                      
+                    });
+                  }, child: Text("=")),
+                  ElevatedButton(onPressed: () {
+                    setState(() {
+                      operaciones += " + ";
+                      estado=true;
+                    });
+                  }, child: Text("+")),
                 ]
               )
 
@@ -75,4 +218,27 @@ class _MyHomePageState extends State<HomePage> {
       ],
     );
   }
+}
+
+int casos(var valor1, String operador,var valor2){
+  var total;
+  print("operacion");
+  print(valor1);
+  print(valor2 );
+  print(operador);
+  switch(operador){
+                        case '-': 
+                          total = valor1 - valor2;
+                        break;
+                        case '+': 
+                          total =  valor1 + valor2;
+                        break;
+                        case '*': 
+                          total =  valor1 * valor2;
+                        break;
+                        case '/': 
+                          total =  valor1 /  valor2;
+                        break;
+                      }
+                      return total;
 }
